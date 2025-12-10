49477fd923aea2bb6adec96725fbace24cdff4b0
Joy <joybrar2001@gmail.com>
2025-12-11 03:09:43 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Navbar from "#components/Navbar";
 import React from "react";
 
 const App = () => {
   return (
-    <h1 className="text-xl text-red-500">Welcome to my MacFolio</h1>
+    <main>
+      <Navbar />
+    </main>
   );
 }
 
