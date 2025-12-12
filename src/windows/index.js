18077973c7b3bdbe94bff1242416895f66abb548
Joy <joybrar2001@gmail.com>
2025-12-13 01:49:39 +0530

    Added the Resume Section with react-pdf

--- src/windows/index.js
+++ src/windows/index.js
@@ -1,7 +1,9 @@
-import Terminal from "./Terminal";
+import Resume from "./Resume";
 import Safari from "./Safari";
+import Terminal from "./Terminal";
 
 export {
-  Terminal,
+  Resume,
   Safari,
+  Terminal,
 };
