6930d1423106ef0656c10ce2bae3e38178ad4f25
Joy <joybrar2001@gmail.com>
2025-12-13 00:02:33 +0530

    Added Window Wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import { Dock, Navbar, Welcome } from "#components";
 import React from "react";
+import gsap from "gsap";
+import { Draggable } from "gsap/Draggable";
+
+import { Dock, Navbar, Welcome } from "#components";
+
+gsap.registerPlugin(Draggable);
 
 const App = () => {
   return (
---
+++ src/hoc/WindowWrapper.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+import useWindowStore from "#store/window";
+
+const WindowWrapper = (Component, windowKey) => {
+  const Wrapped = (props) => {
+    const { focusWindow, windows } = useWindowStore();
+    const { isOpen, zIndex } = windows[windowKey];
+    const ref = useRef(null);
+
+    return (
+      <section id={windowKey} ref={ref} style={{ zIndex }}>
+        <Component {...props} />
+      </section>
+    );
+  }
+
+  Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || "Component"})`
+
+  return Wrapped;
+}
+
+export default WindowWrapper;
