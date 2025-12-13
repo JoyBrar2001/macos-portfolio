e120612f2bfa8d13ae67ac0050c872d604f14200
Joy <joybrar2001@gmail.com>
2025-12-13 05:32:08 +0530

    Improved Finder

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -5,9 +5,27 @@ import { locations } from "#constants";
 import useLocationStore from "#store/location";
 import clsx from "clsx";
 import { WindowControls } from "#components";
+import useWindowStore from "#store/window";
 
 const Finder = () => {
+  const { openWindow } = useWindowStore();
   const { activeLocation, setActiveLocation } = useLocationStore();
+
+  const openItem = (item) => {
+    if (item.fileType === "pdf") {
+      return openWindow("resume");
+    }
+
+    if (item.kind === "folder") {
+      return setActiveLocation(item);
+    }
+
+    if (["fig", "url"].includes(item.fileType) && item.href) {
+      return window.open(item.href, "blank");
+    }
+
+    openWindow(`${item.fileType}${item.kind}`, item);
+  }
 
   const renderList = (name, items) => (
     <div>
@@ -45,9 +63,22 @@ const Finder = () => {
       <div className="bg-white flex h-full">
         <div className="sidebar">
           {renderList("Favorites", Object.values(locations))}
-          
+
           {renderList("Work ", locations.work.children)}
         </div>
+        <ul className="content">
+          {activeLocation?.children.map((item) => (
+            <li
+              key={item.id}
+              className={item.position}
+              onClick={() => openItem(item)}
+            >
+              <img src={item.icon} alt={item.name} />
+              <p>{item.name}</p>
+            </li>
+          ))}
+        </ul>
+
       </div>
     </>
   );
